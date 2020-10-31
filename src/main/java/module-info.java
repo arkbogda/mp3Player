5d28a319045763953c9f64f798b0fa355988ee39
Arek <ark.bogda@gmail.com>
2020-10-31 23:46:20 +0100

    Basic view

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,7 @@ module mp3player {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+
     exports org.example.mp3player.main to javafx.graphics;
+    opens org.example.mp3player.main.controller to javafx.fxml;
 }
---
+++ src/main/java/org/example/mp3player/main/controller/MainController.java
@@ -0,0 +1,46 @@
+package org.example.mp3player.main.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+
+public class MainController {
+
+    @FXML
+    private MenuItem fileMenuItem;
+
+    @FXML
+    private MenuItem dirMenuItem;
+
+    @FXML
+    private MenuItem closeMenuItem;
+
+    @FXML
+    private MenuItem aboutMenuItem;
+
+    @FXML
+    private TableView<?> contentTable;
+
+    @FXML
+    private Button previousButton;
+
+    @FXML
+    private ToggleButton playButton;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    private Slider progressSlider;
+
+    public void initialize() {
+        System.out.println("MainController created");
+    }
+
+}
