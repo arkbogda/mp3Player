095a04522f19c3ff2f94b08fb424d6037b548bea
Arek <ark.bogda@gmail.com>
2020-11-03 16:44:27 +0100

    Controller communication

--- src/main/java/org/example/mp3player/controller/MainController.java
+++ src/main/java/org/example/mp3player/controller/MainController.java
@@ -1,7 +1,22 @@
 package org.example.mp3player.controller;
 
+import javafx.fxml.FXML;
+
 public class MainController {
+
+    @FXML
+    ContentPaneController contentPaneController;
+
+    @FXML
+    ControlPaneController controlPaneController;
+
+    @FXML
+    MenuPaneController menuPaneController;
+
     public void initialize() {
         System.out.println("Main controller created");
+        System.out.println(contentPaneController);
+        System.out.println(controlPaneController);
+        System.out.println(menuPaneController);
     }
 }
