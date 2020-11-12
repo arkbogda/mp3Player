af82ee5f6a3251e46c572fa563cd1a37c4911ca8
Arek <ark.bogda@gmail.com>
2020-11-12 21:51:57 +0100

    JSoup implementation

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module mp3player {
     requires javafx.controls;
     requires javafx.fxml;
 
+
     exports org.example.mp3player.main to javafx.graphics;
     opens org.example.mp3player.controller to javafx.fxml;
+    opens org.example.mp3player.mp3 to javafx.base;
 }
